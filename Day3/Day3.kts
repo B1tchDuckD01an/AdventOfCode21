c9a0ca10c8249dde1d07b0eecd36607922fae5a4
B1tchDuckD01an <elaine.g.dore@gmail.com>
2021-12-03 11:35:39 +0000

    Day3 part 1

---
+++ Day3/Day3.kts
@@ -0,0 +1,52 @@
+import java.io.File
+
+
+val numbers: List<String> = File("input.txt").readLines()
+
+fun parseintolines(input:List<String>)
+{
+
+    fun intArray(str:String):List<Int> = str.map{ it.toString().toInt()}
+
+    fun gamma(list:List<Int>):List<Int> = list.map {
+        if( it > input.size/2 )
+               1
+        else
+        0
+    }
+
+    fun epsilon(list:List<Int>):List<Int> = list.map {
+        if( it  < input.size/2 )
+                1
+        el  se
+            0
+
+    }
+    val inputmap = (0 until input.size).map { i ->
+    intArray(input[i]) }.foldIndexed(mutableListOf<Int>(),{ index, acc, list ->
+        list.mapIndexed {
+            inx, i ->
+                if(index > 0)
+                    acc[inx] = acc[inx] + i
+                else
+                    acc.add(i)
+        }
+        acc
+    }).toList()
+
+    println(gamma(inputmap).joinToString("").toInt(2) * epsilon(inputmap).joinToString("").toInt(2))
+    return
+}
+
+
+
+/*
+fun part1(): {
+    parseintolines()
+}
+
+fun part2(){
+
+}*/
+
+parseintolines(numbers)
