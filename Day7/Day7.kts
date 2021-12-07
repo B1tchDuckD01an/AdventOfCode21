c8f1743cc89beb2c808d6b035c74956e048f54bb
B1tchDuckD01an <elaine.g.dore@gmail.com>
2021-12-07 17:25:50 +0000

    math

---
+++ Day7/Day7.kts
@@ -0,0 +1,45 @@
+import java.io.File
+import kotlin.math.absoluteValue
+
+val numbers = File("input.txt").readText().split(",").map { it.toInt() }
+
+fun fuelcost(i:Int,num:Int):Int = if (i > num) (i - num) else (num - i)
+
+fun part1()
+{
+    var efficientfuel = 999999999
+    for (i in 0..numbers.sorted().last()) {
+        var fuel = 0
+        for (num in numbers) {
+            if (fuel > efficientfuel)
+                break
+            fuel += fuelcost(num,i)
+        }
+        if (fuel < efficientfuel)
+            efficientfuel = fuel
+    }
+    println("----- PART1 ------")
+    println(efficientfuel)
+}
+
+fun part2()
+{
+    var efficientfuel = 999999999
+    for (i in 0..numbers.sorted().last()) {
+        var fuel = 0
+        for (num in numbers) {
+            if (fuel > efficientfuel)
+                break
+            val cost = fuelcost(i,num)
+            fuel += cost*(cost+1)/2
+        }
+        if (fuel < efficientfuel)
+            efficientfuel = fuel
+    }
+    println("----- PART2 ------")
+    println(efficientfuel)
+
+}
+
+part1()
+part2()
