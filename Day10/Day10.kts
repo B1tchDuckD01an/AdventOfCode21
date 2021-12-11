d923a70b2fc54feb9e273fb5fc7573a71bc680b2
B1tchDuckD01an <elaine.g.dore@gmail.com>
2021-12-11 09:53:22 +0000

    scores

--- Day10/Day10.kts
+++ Day10/Day10.kts
@@ -4,13 +4,13 @@ val numbers = File("input.txt").readLines().map { it.map { getPartner(it)}}
 
 fun getPartner(c:Char):Symbol{
     if(c == '<')
-        return Symbol(c,'>',true,0)
+        return Symbol(c,'>',true,4)
     if(c == '{')
-        return Symbol(c,'}',true,0)
+        return Symbol(c,'}',true,3)
     if(c == '(')
-        return Symbol(c,')',true,0)
+        return Symbol(c,')',true,1)
     if(c == '[')
-        return Symbol(c,']',true,0)
+        return Symbol(c,']',true,2)
     if(c == '>')
         return Symbol(c,'<',false,25137)
     if(c == '}')
@@ -29,27 +29,38 @@ class Symbol(val c:Char,val p:Char, val open:Boolean,val score:Int){
     }
 }
 
-fun part1() {
-    val list = mutableListOf<Symbol>()
-    var score : Long = 0
-    for(s in numbers)
-        for(i in 0 .. s.size-1)
-        {
+fun scores() {
+    var score: Long = 0
+    val allscores = mutableListOf<Long>()
+    for (s in numbers) {
+
+        var corrupt = false
+        var score2: Long = 0
+        val list = mutableListOf<Symbol>()
+
+        for (i in 0..s.size - 1) {
             if (s[i].open)
                 list.add(s[i])
-            else
-            {
-                if(list.removeLast().p != s[i].c)
-                {
+            else {
+                if (list.removeLast().p != s[i].c) {
                     score += s[i].score
+                    corrupt = true
                     break
                 }
             }
         }
-
+        if(!corrupt) {
+            for (b in list.reversed()) {
+                score2 = (score2 * 5) + getPartner(b.c).score
+            }
+            allscores.add(score2)
+        }
+    }
     println("---- PART1 -----")
     println(score)
+
+    println("---- PART2 -----")
+    println(allscores.sorted()[allscores.size/2])
 }
 
-part1()
-
+scores()
