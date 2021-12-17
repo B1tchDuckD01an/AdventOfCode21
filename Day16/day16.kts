348aadec3aaaea7e2eabfd493818a6770270e7d2
ElaineDore-OS <elaine.dore@os.uk>
2021-12-17 19:36:30 +0000

    packets

--- Day16/day16.kts
+++ Day16/day16.kts
@@ -1,7 +1,29 @@
 import java.io.File
 import java.math.BigInteger
 
-val numbers = File("test2.txt").readText()
+val numbers = File("test1.txt").readText()
+data class Packet(val binary:String)
+{
+    val version = binary.slice(0..2).toInt(2)
+    val id = binary.slice(3..5).toInt(2)
+    fun literalpacket() = (id == 4)
+
+    fun literalvalue():Int {
+        if(literalpacket())
+        {
+            val numberbinary = binary.slice(6..binary.length-1)
+            val sb = StringBuilder()
+            for(number in numberbinary.windowed(5,5)) {
+                sb.append(number.slice(1..4))
+                if(number[0] == '0')
+                    return sb.toString().toInt(2)
+            }
+        }
+        error("fuck")
+    }
+
+}
+
 
 fun hextobinary(hex:String) = BigInteger(hex,16).toString(2)
 
@@ -18,29 +40,8 @@ fun parseliteralpacket(binary:String):Int
     error("fuck")
 }
 
-
 fun parsepacket(binary:String){
-    val packetv = binary.slice(0..2)
-    val id = binary.slice(3..5)
-    if( id.toInt(2)== 4) {
-        println("literal")
-        println("packet version: $packetv, id: $id")
-        println(parseliteralpacket(binary.slice(6..binary.length-1)))
+    var packet = Packet(binary)
 
-    }
-    else{
-        //operator
-        if(binary[6].digitToInt() == 0)
-        {
-            //15 bit length denoting subpackets
-            val subpacketcount = binary.slice(7..22)
-        }
-        else
-        {
-            val subpacketcount = binary.slice(7..18)
-
-            //11 bit length denoting sub packets
-        }
-    }
 }
 parsepacket(hextobinary(numbers))
